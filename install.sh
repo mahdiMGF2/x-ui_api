2db108136d3ca221a216007b920f1e21286e100a
MahdiGholipour <mahdigholipour@outlook.com>
2023-11-17 19:52:32 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo "installing api"
+git clone https://github.com/mahdigholipour3/x-ui_api.git
+cd x-ui_api
 sudo apt update;
 sudo apt install python-pip;
 pip install -r requirements.txt;
 clear;
 read -p "Enter IP Address: " ip;
 uvicorn main:app --host $ip --port 8000 --reload
-echo "installed"
