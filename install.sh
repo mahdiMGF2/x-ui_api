d6fbd94e0ded717af9422ad451bb1424e5fe72b1
MahdiGholipour <mahdigholipour@outlook.com>
2023-11-18 00:20:43 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ git clone https://github.com/mahdigholipour3/x-ui_api.git
 cd x-ui_api
 sudo apt update;
 sudo apt install python3-pip;
-clear;
 pip3 install -r requirements.txt;
-read -p "IP Address or domain: " ip;
-uvicorn main:app --host $ip --port 8000 --reload
+clear;
+echo "installed api";
 
+ nohup python3 -u main.py
